e66f7bd01a2c507a083c0c7cc528329c45d4a829
Aivar Mägi <aivar.magi@itella.com>
2024-01-03 20:05:45 +0200

    Add @Crossorigin to controller

--- api/src/main/java/ee/aivar/filters/controller/FilterController.java
+++ api/src/main/java/ee/aivar/filters/controller/FilterController.java
@@ -3,6 +3,7 @@ package ee.aivar.filters.controller;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import ee.aivar.filters.service.FilterService;
 
 import io.swagger.v3.oas.annotations.Operation;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/filters")
 public class FilterController {
